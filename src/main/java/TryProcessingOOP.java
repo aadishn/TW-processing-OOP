9afaae6021b36cad7f23e49405c7a63ea3f0bb72
aadishazzam <aadishazzam@gmail.com>
2021-01-07 21:55:42 +0530

    #00 Refactor. refactor variables and resolve breaking abstraction

--- src/main/java/TryProcessingOOP.java
+++ src/main/java/TryProcessingOOP.java
@@ -3,16 +3,29 @@ import processing.core.PApplet;
 public class TryProcessingOOP extends PApplet {
     public static final int WIDTH = 700;
     public static final int HEIGHT = 700;
-    public static final int majorAxis = 15;
-    public static final int minorAxis = 15;
-    public static final int numberOfBalls = 4;
-    public static final int whiteBackground = 255;
-    public static int ballSpeed = 0;
+    public static final int MAJOR_AXIS = 15;
+    public static final int MINOR_AXIS = 15;
+    public static final int NUMBER_OF_BALLS = 4;
+    public static final int WHITE_BACKGROUND = 255;
+    public static final int X_POSITION = 0;
+    public static int INITIAL_BALL_SPEED = 0;
 
     public static class RollingBall {
         int xPosition;
         int yPosition;
         int ballSpeed;
+
+        public int getxPosition() {
+            return xPosition;
+        }
+
+        public void setxPosition() {
+            this.xPosition += ballSpeed;
+        }
+
+        public int getyPosition() {
+            return yPosition;
+        }
 
         public RollingBall(int xPosition, int yPosition, int ballSpeed) {
             this.xPosition = xPosition;
@@ -21,7 +34,7 @@ public class TryProcessingOOP extends PApplet {
         }
     }
 
-    RollingBall [] Balls;
+    RollingBall [] balls;
 
     public static void main(String[] args) {
         PApplet.main("TryProcessingOOP", args);
@@ -35,25 +48,25 @@ public class TryProcessingOOP extends PApplet {
 
     @Override
     public void setup() {
-        background(whiteBackground);
-        Balls = new RollingBall[numberOfBalls];
-        for (int ballIndex = 0; ballIndex < numberOfBalls; ballIndex++) {
-            Balls[ballIndex] = new RollingBall(0, (HEIGHT * (ballIndex+1)) / 5, ++ballSpeed);
+        background(WHITE_BACKGROUND);
+        balls = new RollingBall[NUMBER_OF_BALLS];
+        for (int ballIndex = 0; ballIndex < NUMBER_OF_BALLS; ballIndex++) {
+            balls[ballIndex] = new RollingBall(X_POSITION, (HEIGHT * (ballIndex+1)) / 5, ++INITIAL_BALL_SPEED);
         }
     }
 
     @Override
     public void draw() {
         int ballIndex = 0;
-        while (ballIndex < numberOfBalls) {
-            drawingEllipse(Balls[ballIndex]);
+        while (ballIndex < NUMBER_OF_BALLS) {
+            drawEllipse(balls[ballIndex]);
             ballIndex++;
         }
     }
 
-    public void drawingEllipse(RollingBall ball) {
-        ellipse(ball.xPosition, ball.yPosition, majorAxis, minorAxis);
-        ball.xPosition += ball.ballSpeed;
+    public void drawEllipse(RollingBall ball) {
+        ellipse(ball.getxPosition(), ball.getyPosition(), MAJOR_AXIS, MINOR_AXIS);
+        ball.setxPosition();
     }
 }
 
